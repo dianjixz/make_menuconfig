4e7d178e98f32a04e3cc1b12a4eb7b26d6e02316
sknown <huirong.lin@qq.com>
2018-06-12 08:10:27 -0700

    add install script

---
+++ tiny/myconfig.mk
@@ -0,0 +1,41 @@
+PHONY += menuconfig silentoldconfig clean distclean
+
+Kconfig      := Kconfig
+rm-distclean += include/generated include/config  .config
+
+export SRC_ROOT := $(shell pwd)
+export CFLAGS += -I$(SRC_ROOT)/include/
+
+
+
+ifeq ($(quiet),silent_)
+silent := -s
+endif
+
+%:: FORCE
+
+ALL:
+
+menuconfig : mconf conf
+	$(Q)./$< $(Kconfig)
+	$(Q)$(MAKE) silentoldconfig
+
+%_defconfig:  
+	$(Q)echo "$@ ..."
+	$(Q)$(MAKE) -C configs $@
+	$(Q)$(MAKE) menuconfig
+
+saveconfig:
+	$(Q)$(MAKE) -C configs save
+
+silentoldconfig: conf
+	$(Q)mkdir -p include/generated include/config
+	$(Q)./$< -s --silentoldconfig $(Kconfig)
+
+distclean:clean
+	-rm -rf  $(rm-distclean)
+
+FORCE:
+
+.PHONY : ALL FORCE
+
